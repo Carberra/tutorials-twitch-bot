f57e11c92190c30aec710e00574a75d6d407cf4f
parafoxia <parafoxia@carberra.xyz>
2020-12-17 01:40:27 +0000

    Added help system
    Added help cog, complete with help command.

---
+++ carberretta/bot/cogs/help.py
@@ -0,0 +1,18 @@
+from twitchio.ext import commands
+
+
+class Help:
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command(name="help")
+    async def help_command(self, ctx: commands.bot.Context) -> None:
+        await ctx.send(
+            "Commands: {}".format(
+                " • ".join("|".join([cmd.name, *(cmd.aliases or [])]) for cmd in self.bot.commands.values())
+            )
+        )
+
+
+def prepare(bot: commands.Bot) -> None:
+    bot.add_cog(Help(bot))
